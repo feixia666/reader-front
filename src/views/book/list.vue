c721e6c29ad5a6373d2221aa641b9b77c639b4d2
772809720@qq.com <772809720@qq.com>
2020-02-18 15:59:01 +0800

    图书列表开发

--- src/views/book/list.vue
+++ src/views/book/list.vue
@@ -59,13 +59,100 @@
       <el-table-column label="书名"
         width="150"
         align="center">
-        <template slot-scope="{ row: {title}}">
-          <span>{{title}}</span>
+        <template slot-scope="{ row: {titleWrapper}}">
+          <span v-html="titleWrapper" />
         </template>
       </el-table-column>
-
+      <el-table-column label="作者"
+        width="150"
+        align="center">
+        <template slot-scope="{ row: {authorWrapper}}">
+          <span v-html="authorWrapper" />
+        </template>
+      </el-table-column>
+      <el-table-column label="出版社"
+        prop="publisher"
+        align="center"
+        width="150"></el-table-column>
+      <el-table-column label="分类"
+        prop="categoryText"
+        align="center"
+        width="150"></el-table-column>
+      <el-table-column label="语言"
+        prop="language"
+        align="center"
+        width="150"></el-table-column>
+      <el-table-column v-if="showCover"
+        label="封面"
+        prop="categoryText"
+        align="center"
+        width="150">
+        <template slot-scope="scope"><a :href="scope.row.cover"
+            target="_blank">
+            <img :src="scope.row.cover"
+              style="width: 120px; height: 180px;">
+          </a></template>
+      </el-table-column>
+      <el-table-column label="文件名"
+        prop="fileName"
+        align="center"
+        width="100"></el-table-column>
+      <el-table-column label="文件路径"
+        prop="filePath"
+        align="center"
+        width="100">
+        <template slot-scope="{ row: { filePath }}">
+          <span>{{ filePath | valueFilter }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="封面路径"
+        prop="coverPath"
+        align="center"
+        width="100">
+        <template slot-scope="{ row: { coverPath }}">
+          <span>{{ coverPath | valueFilter }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="解压路径"
+        prop="unzipPath"
+        align="center"
+        width="100">
+        <template slot-scope="{ row: { unzipPath }}">
+          <span>{{ unzipPath | valueFilter }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="上传人"
+        prop="createUser"
+        align="center"
+        width="100">
+        <template slot-scope="{ row: { createUser }}">
+          <span>{{ createUser | valueFilter }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="上传时间"
+        prop="createDt"
+        align="center"
+        width="100">
+        <template slot-scope="{ row: { createDt }}">
+          <span>{{ createDt | timeFilter }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作"
+        width="120"
+        align="center"
+        fixed="right">
+        <template slot-scope="scope">
+          <el-button type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row.fileName)"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
-    <pagination :total="0" />
+    <pagination v-show="total > 0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.pageSize"
+      @pagination="handleFilter" />
   </div>
 </template>
 
@@ -73,6 +160,7 @@
 import pagination from '../../components/Pagination'
 import waves from '../../directive/waves'
 import { getCategory, listBook } from '../../api/book'
+import { parseTime } from '../../utils'
 export default {
   components: {
     pagination
@@ -80,34 +168,64 @@ export default {
   directives: {
     waves
   },
+  filters: {
+    valueFilter(value) {
+      return value || '无'
+    },
+    timeFilter(time) {
+      return time ? parseTime(time, '{y}-{m}-{d} {h}:{i}') : '无'
+    }
+  },
   data() {
     return {
       tableKey: 0,
       listLoading: false,
-      listQuery: {
-        title: '',
-        author: '',
-        category: ''
-      },
+      listQuery: {},
       list: '',
-      category: [
-        {
-          value: '选项1',
-          label: '黄金糕'
-        }
-      ],
-      showCover: false
+      category: [],
+      showCover: false,
+      total: 0
     }
+  },
+  created() {
+    this.parseQuery()
   },
   mounted() {
     this.getList()
     this.getCategoryList()
   },
   methods: {
+    parseQuery() {
+      let listQuery = {
+        page: 1,
+        pageSize: 20,
+        sort: '+id'
+      }
+      this.listQuery = { ...listQuery, ...this.listQuery }
+    },
+    wrapperKeyword(key, value) {
+      function hightlight(value) {
+        return `<span style="color: #1890ff">${value}</span>`
+      }
+      if (!this.listQuery[key]) {
+        return value
+      } else {
+        return value.replace(new RegExp(this.listQuery[key], 'ig'), value =>
+          hightlight(value)
+        )
+      }
+      this.listQuery[key]
+    },
     getList() {
       this.listLoading = true
       listBook(this.listQuery).then(res => {
-        this.list = res.data
+        this.list = res.data.list
+        this.total = res.data.count
+        this.listLoading = false
+        this.list.forEach(book => {
+          book.titleWrapper = this.wrapperKeyword('title', book.title)
+          book.authorWrapper = this.wrapperKeyword('author', book.author)
+        })
       })
     },
     getCategoryList() {
@@ -115,15 +233,26 @@ export default {
         this.category = res.data
       })
     },
-    sortChange() {
-      console.log('sortchange')
+    sortChange(data) {
+      const { prop, order } = data
+      this.sortBy(prop, order)
+    },
+    sortBy(prop, order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = `+${prop}`
+      } else {
+        this.listQuery.sort = `-${prop}`
+      }
+      this.handleFilter()
     },
     handleFilter() {
-      console.log('keyup')
       this.getList()
     },
     handleCreate() {
       this.$router.push({})
+    },
+    handleUpdate(fileName) {
+      this.$router.push(`/book/edit/${fileName}`)
     },
     changeShowCover(value) {
       this.showCover = value
